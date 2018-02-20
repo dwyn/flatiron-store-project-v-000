b4d42943f2836beb65f49b6d24d64cfa7128891b
dwyn <dwayne.harmon@gmail.com>
2018-02-19 22:04:27 -0600

    P r o g r e s s

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180220035935) do
+ActiveRecord::Schema.define(version: 20180220040309) do
 
   create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
