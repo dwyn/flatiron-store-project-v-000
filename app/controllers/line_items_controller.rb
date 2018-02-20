5386278f54198b99f44e003f3c2de8c838cdea7c
dwyn <dwayne.harmon@gmail.com>
2018-02-19 22:44:47 -0600

    Done.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,3 +1,17 @@
 class LineItemsController < ApplicationController
-
+  def create
+    if current_user.current_cart.nil? || current_user.current_cart.status == "submitted"
+      @cart = Cart.create(user_id: current_user.id)
+      current_user.update(current_cart: @cart)
+    else
+      @cart = current_cart
+    end
+    line_item = @cart.add_item(params[:item_id])
+    if line_item.save
+      @cart.save
+      redirect_to cart_path(@cart)
+    else
+      redirect_to root_path
+    end
+  end
 end
